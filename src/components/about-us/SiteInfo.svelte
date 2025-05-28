e644c2af05a2266eef43cb7d59b844ef456dae02
Jack Maddigan <jack@MacBookPro.localdomain>
2025-05-28 12:35:00 +1200

    fix misspelt name

--- src/components/about-us/SiteInfo.svelte
+++ src/components/about-us/SiteInfo.svelte
@@ -1,7 +1,7 @@
 <h2>SITE INFO</h2>
 <p>
   Photos provided by 
-  <a href="https://www.instagram.com/corysonntag.photography/">Cory Sontag</a>,
+  <a href="https://www.instagram.com/corysonntag.photography/">Cory Sonntag</a>,
   <a href="https://www.instagram.com/lint.___/?hl=en">Lint</a>,
   <a href="https://www.instagram.com/cyansandwich/">Tom Nelson</a>, and
   <a href="https://www.instagram.com/nmak.jpg">Noelle Mak</a>. 
