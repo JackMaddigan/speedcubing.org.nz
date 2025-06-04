367504f32f544679a045b3933336c75bfcc3e431
Jack Maddigan <jack@MacBookPro.localdomain>
2025-06-04 19:59:27 +1200

    infinite scroll carousel

--- src/components/home/Carousel.svelte
+++ src/components/home/Carousel.svelte
@@ -1,102 +1,183 @@
 <script>
   import "bootstrap-icons/font/bootstrap-icons.css";
-  import { onMount } from "svelte";
-  let slides;
-  let index = 0;
+  import { tick } from "svelte";
+
   const imgSrcs = [
-    "gallery/gallery1.jpg",
-    "gallery/gallery2.jpg",
     "gallery/gallery3.jpg",
+    "gallery/gallery2.jpg",
     "gallery/gallery5.webp",
     "gallery/gallery6.webp",
     "gallery/gallery7.webp",
-    "gallery/gallery8.webp",
+    "gallery/gallery9.webp",
   ];
+
   let interval = setInterval(next, 5000);
-
-  function next() {
-    index = (index + 1) % imgSrcs.length;
-    resetInterval();
-  }
-
-  function prev() {
-    index = (index - 1 + imgSrcs.length) % imgSrcs.length;
-    resetInterval();
-  }
-
   function resetInterval(){
     clearInterval(interval);
     interval = setInterval(next, 5000);
   }
 
-  $: offset = `translateX(-${index * 100}%)`;
-  let carousel;
+  // If slide is prev or active, do the 0.5s transition, else transition instantly
+  let lastActive = $state(0);
+  let active = $state(0);
 
-  onMount(() => {
-    if(window.innerWidth <= 768){ carousel.style.height = `calc(${window.innerHeight}px - 280px)`}
-    else { carousel.style.height = `calc(${window.innerHeight}px - 180px)`; }
-  })
+  // initialise transforms for each image to 0
+  // except last, which needs to be to the left of the first slide
+  let transforms = $state(imgSrcs.map((_, i) =>  i === imgSrcs.length - 1 ? -100*imgSrcs.length : 0));
+
+  async function handleClick(goal){
+    let diff = goal - active;
+      for(let i=0; i<Math.abs(diff); i++){
+        if(diff < 0){ prev(); }
+        else { next(); }
+      }
+  }
+
+  /* Move the carousel forwards */
+  function next(){
+    if(active === imgSrcs.length - 2){
+      // arriving to the last slide
+      // move all but the first to the left by 1
+      for(let i=1; i<imgSrcs.length; i++){ transforms[i] -= 100; }
+      // move first to its original position + 100%
+      transforms[0] = 100;
+    }else if(active === imgSrcs.length - 1) {
+      // arriving to the first slide from last
+      // move all but the last to their correct positions. 
+      for(let i=0; i<imgSrcs.length-1; i++){ transforms[i] = 0; }
+      // move the last one off to the left again
+      transforms[imgSrcs.length - 1] -= 100;
+    }else if(active === 0){
+      // arriving to second slide from first
+      // ensure that all transitions are -100%
+      transforms = transforms.map(_ => -100);
+    }else{
+      // somewhere in the middle, move all to the left by 100%
+      transforms = transforms.map(e => e -=100);
+    }
+    lastActive = active;
+    active = (active+1) % imgSrcs.length;
+    resetInterval();
+  }
+
+  /* Move the carousel backwards */
+  function prev(){
+    if(active === imgSrcs.length - 1){
+      // arriving to the second to last slide from the last
+      transforms = transforms.map((_, i) => (imgSrcs.length - 2)*-100);
+    }else if(active === 0){
+      // arriving to the last slide from the first
+      // move last and first right by 100%
+      transforms[0] += 100;
+      transforms[imgSrcs.length - 1] += 100;
+      // move the rest to off the left
+      for(let i=1; i<imgSrcs.length - 1; i++){ transforms[i] = -(i+1)*100; }
+    }else if(active === 1){
+      // moving from the second to the first
+      // ensure last slide is to the left
+      transforms[imgSrcs.length - 1] = imgSrcs.length * -100;
+      for(let i=0; i<imgSrcs.length - 1; i++){ transforms[i] += 100; }
+    }else{
+      // somewhere in the middle, move all to the left by 100%
+      transforms = transforms.map(e => e +=100);
+    }
+    lastActive = active;
+    active --;
+    if(active < 0){ active = imgSrcs.length - 1; }
+    resetInterval();
+  }
+
 </script>
 
-<div class="carousel" bind:this={carousel}>
-  <div class="slides" style:transform={offset}>
-    {#each imgSrcs as src}
-      <img src={src} alt="">
+<section class="carousel">
+  <div class="carousel_items">
+    {#each imgSrcs as src, i}
+      <div class="carousel_item" 
+        style="transform: translateX({transforms[i]}%); transition: transform {(i==active || i==lastActive) ? "0.5s" : "0s"} ease;">
+        <img src={src} alt="{i} of {imgSrcs.length}">
+      </div>
     {/each}
   </div>
-
-  <button on:click={prev} class="left" aria-label="left"
-    ><i class="bi bi-chevron-left"></i></button
-  >
-  <button on:click={next} class="right" aria-label="right"
-    ><i class="bi bi-chevron-right"></i></button
-  >
-</div>
+  <div class="nav_dots">
+    {#each imgSrcs as _, i}
+      <button class="nav_dot" aria-label="nav dot" class:active_dot={i === active} onclick={() => handleClick(i)}></button>
+    {/each}
+  </div>
+  <button onclick={prev} aria-label="next" class="left"><i class="bi bi-chevron-left"></i></button>
+  <button onclick={next} aria-label="next" class="right"><i class="bi bi-chevron-right"></i></button>
+</section>
 
 <style>
-  .carousel {
-    overflow: hidden;
-    position: relative;
-    background-color: var(--colorBlack2);
-    touch-action: manipulation;
-    height: calc(100vh - 180px); /* will get overridden by onmount, but as default this is good */
-  }
-
-  .slides{
-    display: flex;
-    flex-direction: row;
-    transition: transform 1s ease;
-    height: 100%;
-    width: 100%;
-  }
- 
-  img {
-    object-fit: cover;
-    object-position: center;
-    min-width: 100%;
-    height: 100%;
-  }
-
-  button {
-    background: none;
-    border: none;
-    color: var(--colorWhite);
+  .nav_dots {
     position: absolute;
-    top: 50%;
-    font-size: 32px;
+    bottom: 12px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    gap: 12px;
+  }
+  .nav_dot {
+    width: 16px;
+    height: 16px;
+    background-color: #9d9d9d;
+    border-radius: 50%;
+    border: none;
+    opacity: 0.7;
+    transition: color 0.5s ease;
     cursor: pointer;
   }
-
-  .left {
-    left: 2%;
+  .nav_dot:hover{
+    background-color: #fff;
   }
-  .right {
-    right: 2%;
+  .active_dot{
+    background-color: #fff;
+    opacity: 1;
+  }
+  .carousel {
+    width: 100%;
+    height: calc(100vh - 200px);
+    overflow: hidden;
+    position: relative;
+  }
+  .carousel_items {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+  }
+  .carousel_item {
+    width: 100%;
+    height: 100%;
+    flex: 0 0 auto;
+  }
+  .carousel_item img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: center;
+  }
+  .left, .right {
+    padding: 0px;
+    border: none;
+    background-color: transparent;
+    color: #fff;
+    font-size: clamp(2.3rem, 2vw, 3rem);
+    cursor: pointer;
+    position: absolute;
+    top: 50%;
+    touch-action: manipulation;
+  }
+  .left {
+    left: 5px;
+  }
+  .right{ 
+    right: 5px;
   }
 
   @media screen and (max-width: 768px){
     .carousel {
-      height: calc(100vh - 280px); /* will get overridden by onmount but as default this is good */
+      height: 100vw;
+      max-height: 70vh;
     }
   }
 </style>
