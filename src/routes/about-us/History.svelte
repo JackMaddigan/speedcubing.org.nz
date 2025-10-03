d68be60039ebcadf4dedc4b30731ee8e02b37a8c
Oscar Welton-Rosie <oweltonrosie@gmail.com>
2025-10-03 15:12:51 +1300

    make external links on the about page consistently open in a new tab

--- src/routes/about-us/History.svelte
+++ src/routes/about-us/History.svelte
@@ -1,110 +1,110 @@
 <section>
   <h2>HISTORY</h2>
   <p>
-    In 2009, <a href="https://www.worldcubeassociation.org/persons/2009BRUN03">Joshua Brungar</a>
-    and <a href="https://www.worldcubeassociation.org/persons/2009BEAR01">Dene Beardsley</a> organised
-    New Zealand’s first official <a href="https://www.worldcubeassociation.org/competitions/NewZealandChamps2009">
+    In 2009, <a href="https://www.worldcubeassociation.org/persons/2009BRUN03" target="_blank">Joshua Brungar</a>
+    and <a href="https://www.worldcubeassociation.org/persons/2009BEAR01" target="_blank">Dene Beardsley</a> organised
+    New Zealand's first official <a href="https://www.worldcubeassociation.org/competitions/NewZealandChamps2009" target="_blank">
       speedcubing competition</a>. Held in Te Papa, it drew eyes to speedcubing for the first time in New Zealand
       history, receiving attention from several news outlets
-      (<a href="https://wellington.scoop.co.nz/?p=8155">1</a>,
-      <a href="https://web.archive.org/web/20121119133721/http://www.3news.co.nz/News/Kiwi-Rubiks-cube-champs-to-be-held-in-Wellington/tabid/418/articleID/112620/cat/55/Default.aspx">2</a>,
-      <a href="https://www.stuff.co.nz/dominion-post/weird-news/2561010/Josh-warms-up-for-cuber-speed-carnival">3</a>,
-      <a href="https://www.stuff.co.nz/dominion-post/wellington/2608244/Rubiks-cubers-a-breed-apart">4</a>).
-      This also marked Oceania’s first competition, and saw the debut of
-      <a href="https://www.worldcubeassociation.org/persons/2009ZEMD01">Feliks Zemdegs</a>
+      (<a href="https://wellington.scoop.co.nz/?p=8155" target="_blank">1</a>,
+      <a href="https://web.archive.org/web/20121119133721/http://www.3news.co.nz/News/Kiwi-Rubiks-cube-champs-to-be-held-in-Wellington/tabid/418/articleID/112620/cat/55/Default.aspx" target="_blank">2</a>,
+      <a href="https://www.stuff.co.nz/dominion-post/weird-news/2561010/Josh-warms-up-for-cuber-speed-carnival" target="_blank">3</a>,
+      <a href="https://www.stuff.co.nz/dominion-post/wellington/2608244/Rubiks-cubers-a-breed-apart" target="_blank">4</a>).
+      This also marked Oceania's first competition, and saw the debut of
+      <a href="https://www.worldcubeassociation.org/persons/2009ZEMD01" target="_blank">Feliks Zemdegs</a>
       (who would go on to become the greatest cuber of all time, setting over a hundred world records,
       winning multiple world titles, and even starring in a Netflix documentary).<br/><br/>
   
     The following year we held three competitions, including the
-    <a href="https://www.worldcubeassociation.org/competitions/NewZealand2010">National Championship</a>
+    <a href="https://www.worldcubeassociation.org/competitions/NewZealand2010" target="_blank">National Championship</a>
     returning to Te Papa. This edition was even bigger and better, with more competitors, faster results,
     and Feliks attending once again. A special highlight was his
-    <a href="https://www.youtube.com/watch?v=Sa8owbBOvmQ&ab_channel=FeliksZemdegs">8.52 second 3x3 average</a>
+    <a href="https://www.youtube.com/watch?v=Sa8owbBOvmQ&ab_channel=FeliksZemdegs" target="_blank">8.52 second 3x3 average</a>
     in finals - winning the competition with a new World Record, the first sub-9 second average in history!<br/><br/>
   
     After the excitement of those first years though, New Zealand interest in speedcubing seemed to wane.
-    <a href="https://www.worldcubeassociation.org/persons/2010LIMD01">David Lim</a> was appointed as a
-    new delegate, but despite his and Joshua’s efforts, it seemed that the hype had worn off for many people.
+    <a href="https://www.worldcubeassociation.org/persons/2010LIMD01" target="_blank">David Lim</a> was appointed as a
+    new delegate, but despite his and Joshua's efforts, it seemed that the hype had worn off for many people.
     2013 marked our smallest year ever, with only one competition, and just 19 competitors.<br/><br/>
   
-    However, even if the novelty was gone, speedcubing wasn’t going anywhere. The sport was steadily growing
+    However, even if the novelty was gone, speedcubing wasn't going anywhere. The sport was steadily growing
     in popularity worldwide, and it was only a matter of time before interest picked up here too. 2014 and
     2015 saw gradual growth, and by 2016, we had really gained momentum. Competitor numbers were rising, so
-    <a href="https://www.worldcubeassociation.org/persons/2013ASBE01">Alex Asbery</a> was appointed as a
+    <a href="https://www.worldcubeassociation.org/persons/2013ASBE01" target="_blank">Alex Asbery</a> was appointed as a
     new delegate, and we held 5 competitions, our most so far! The year was capped off with Nationals in
     December, which was special in two ways - it was our largest competition at the time with 79 competitors,
     and Joshua Brungar took the opportunity to found Speedcubing New Zealand as an incorporated society.<br/><br/>
   
     Under Speedcubing New Zealand, and with Alex delegating, the community here would continue to flourish
-    over the following years. <a href="https://www.worldcubeassociation.org/competitions/WC2017">The 2017
+    over the following years. <a href="https://www.worldcubeassociation.org/competitions/WC2017" target="_blank">The 2017
     World Championship</a> in Paris saw several kiwi competitors travel to compete... including
-    <a href="https://www.worldcubeassociation.org/persons/2013NELS01">Tom Nelson</a>, who managed to win
+    <a href="https://www.worldcubeassociation.org/persons/2013NELS01" target="_blank">Tom Nelson</a>, who managed to win
     5x5 Blindfolded and become our first World Champion! The next year,
-    <a href="https://www.worldcubeassociation.org/persons/2015MACD03">James Macdiarmid</a> was appointed
+    <a href="https://www.worldcubeassociation.org/persons/2015MACD03" target="_blank">James Macdiarmid</a> was appointed
     as a delegate, and in 2019, he and Alex would both be part of the delegate team for the
-    <a href="https://www.worldcubeassociation.org/competitions/WC2019">World Championship</a>, held in
+    <a href="https://www.worldcubeassociation.org/competitions/WC2019" target="_blank">World Championship</a>, held in
     Melbourne. These years also saw some of our top talents join the community, with cubers like
-    <a href="https://www.worldcubeassociation.org/persons/2019RAMO05">Dwyane Ramos</a>,
-    <a href="https://www.worldcubeassociation.org/persons/2018MURR03">Jasper Murray</a>,
-    <a href="https://www.worldcubeassociation.org/persons/2018MATY02">Adrien Auvray Matyn</a>,
-    <a href="https://www.worldcubeassociation.org/persons/2018EYOU02">Elyas Eyou</a>, and
-    <a href="https://www.worldcubeassociation.org/persons/2019VUJC01">Alexander Vujcich</a>
+    <a href="https://www.worldcubeassociation.org/persons/2019RAMO05" target="_blank">Dwyane Ramos</a>,
+    <a href="https://www.worldcubeassociation.org/persons/2018MURR03" target="_blank">Jasper Murray</a>,
+    <a href="https://www.worldcubeassociation.org/persons/2018MATY02" target="_blank">Adrien Auvray Matyn</a>,
+    <a href="https://www.worldcubeassociation.org/persons/2018EYOU02" target="_blank">Elyas Eyou</a>, and
+    <a href="https://www.worldcubeassociation.org/persons/2019VUJC01" target="_blank">Alexander Vujcich</a>
     all starting their cubing careers in this period.<br/><br/>
   
     2020 brought the COVID-19 pandemic, which put a stop to competitions worldwide. This was almost a
     blessing in disguise for the cubing community however - with so many people stuck at home,
-    cubing was a perfect hobby to pick up. Netflix’s documentary
-    <a href="https://www.netflix.com/nz/title/81092143">The Speed Cubers</a> was released during
+    cubing was a perfect hobby to pick up. Netflix's documentary
+    <a href="https://www.netflix.com/nz/title/81092143" target="_blank">The Speed Cubers</a> was released during
     the lockdowns as well, which brought cubing to a huge mainstream audience like never before.
     With so many people getting into cubing and no competitions for them to attend, demand was
     pushed to an all-time high. When competitions started to return around the world, we saw
     an explosion of popularity, with registrations often filling in a matter of minutes.<br/><br/>
   
-    New Zealand’s handling of the pandemic meant that restrictions were lifted here before much
+    New Zealand's handling of the pandemic meant that restrictions were lifted here before much
     of the rest of the world. We appointed
-    <a href="https://www.worldcubeassociation.org/persons/2018HEAT01">Luke Heath-Edwards</a>
+    <a href="https://www.worldcubeassociation.org/persons/2018HEAT01" target="_blank">Luke Heath-Edwards</a>
     as a new delegate, and by late 2020 were able to hold regular competitions again.
     This proved to be a transformative period for us, with many of our top cubers developing
     into prodigious talents. 2021 saw Dwyane and Alexander breaking their first Oceanic Records,
     both of which had previously been held by Feliks Zemdegs. We also saw Jasper set a
-    <a href="https://www.youtube.com/watch?v=IOAVKxQzDYw&ab_channel=JasperMurray">
-      1.66 second Pyraminx average</a> - giving us New Zealand’s first ever World Record!<br/><br/>
+    <a href="https://www.youtube.com/watch?v=IOAVKxQzDYw&ab_channel=JasperMurray" target="_blank">
+      1.66 second Pyraminx average</a> - giving us New Zealand's first ever World Record!<br/><br/>
   
     Since then, the New Zealand speedcubing community has continued to foster world-class cubers,
     and has consistently ranked in the top 10 countries worldwide for overall performance. In 2022,
     the post-COVID cubing boom produced our largest competition ever -
-    <a href="https://www.worldcubeassociation.org/competitions/NewZealandNationals2022">Nationals</a>
+    <a href="https://www.worldcubeassociation.org/competitions/NewZealandNationals2022" target="_blank">Nationals</a>
     was held in Auckland, with a staggering 268 competitors. A week after that, Melbourne hosted the
-    inaugural <a href="https://www.worldcubeassociation.org/competitions/OC2022">Oceanic Championship</a>.
+    inaugural <a href="https://www.worldcubeassociation.org/competitions/OC2022" target="_blank">Oceanic Championship</a>.
     Kiwis took out many of the top spots, and Dwyane managed to win 3x3 against a super competitive field,
      becoming the first ever Oceanic Champion!
   
     In 2023 and 2024, we continued to host more and more competitions to meet the increased demand.
     We appointed many new delegates, including
-    <a href="https://www.worldcubeassociation.org/persons/2015XIET01">Tim Xie</a>,
-    <a href="https://www.worldcubeassociation.org/persons/2020FRAS02">Daniel Fraser</a>,
-    <a href="https://www.worldcubeassociation.org/persons/2019HALL04">Caleb Hall</a>,
-    <a href="https://www.worldcubeassociation.org/persons/2017WADE01"> Liam Wadek</a>,
-    <a href="https://www.worldcubeassociation.org/persons/2016TRAS01">Finn Trass</a>,
-    <a href="https://www.worldcubeassociation.org/persons/2020MADD02">Jack Maddigan</a>, and
-    <a href="https://www.worldcubeassociation.org/persons/2019DYER01">James Dyer</a>.
+    <a href="https://www.worldcubeassociation.org/persons/2015XIET01" target="_blank">Tim Xie</a>,
+    <a href="https://www.worldcubeassociation.org/persons/2020FRAS02" target="_blank">Daniel Fraser</a>,
+    <a href="https://www.worldcubeassociation.org/persons/2019HALL04" target="_blank">Caleb Hall</a>,
+    <a href="https://www.worldcubeassociation.org/persons/2017WADE01" target="_blank"> Liam Wadek</a>,
+    <a href="https://www.worldcubeassociation.org/persons/2016TRAS01" target="_blank">Finn Trass</a>,
+    <a href="https://www.worldcubeassociation.org/persons/2020MADD02" target="_blank">Jack Maddigan</a>, and
+    <a href="https://www.worldcubeassociation.org/persons/2019DYER01" target="_blank">James Dyer</a>.
     Several of our best cubers travelled to the 2023 World Championship in South Korea,
     performing very well. Particularly impressive was Alexander taking a bronze medal in Megaminx,
     his main event. After that, 2024 was another breakthrough year for us on many fronts.
-    <a href="https://www.worldcubeassociation.org/persons/2021SHEN01">Edwin Shen</a>
-    opened it up with a <a href="https://www.youtube.com/watch?v=EbPMPCSp0QA&ab_channel=EdwinShen">
+    <a href="https://www.worldcubeassociation.org/persons/2021SHEN01" target="_blank">Edwin Shen</a>
+    opened it up with a <a href="https://www.youtube.com/watch?v=EbPMPCSp0QA&ab_channel=EdwinShen" target="_blank">
       3.47 World Record</a> average in Clock, becoming the second ever kiwi
     World Record holder. We organised 33 competitions all across the country, including in 5 new towns.
     And the year closed with the second edition of the
-    <a href="https://www.worldcubeassociation.org/competitions/OC2024">Oceanic Championship</a>,
+    <a href="https://www.worldcubeassociation.org/competitions/OC2024" target="_blank">Oceanic Championship</a>,
     this time in Perth. Our New Zealand contingent proved dominant, winning over half the gold medals,
     and Dwyane managed to defend his title in 3x3!<br/><br/>
   
-    Since Speedcubing New Zealand was founded, the community here has changed immensely. We’ve grown a 
+    Since Speedcubing New Zealand was founded, the community here has changed immensely. We've grown a 
     tonne, with more demand, more comps, and an incredible amount of world-class cubers emerging. 
-    <a href="https://www.worldcubeassociation.org/persons/2022GIBS04">Lachlan Gibson</a> has proven 
+    <a href="https://www.worldcubeassociation.org/persons/2022GIBS04" target="_blank">Lachlan Gibson</a> has proven 
     to be our latest rising star, blowing us all away with a series of World Records in Clock. 
-    We’ve also seen so many new faces join the community who can bring experience and fresh ideas 
+    We've also seen so many new faces join the community who can bring experience and fresh ideas 
     to help run it. In early 2025, Speedcubing New Zealand (2025) was registered as the new 
     regional organisation for New Zealand, in order to better serve the community here. We hope
     that with new leadership and a larger team, we can serve the 
